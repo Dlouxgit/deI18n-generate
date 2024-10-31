9f3c7cb8ac1575e32a974cea94b468d4b3a140b2
Dlouxgit <470490778@qq.com>
2024-10-31 14:33:04 +0800

    feat: add bin

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
-import path from 'path';
-import axios from 'axios';
-import process from 'process';
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
+import axios from 'axios'
 
-const rootUrl = process.argv[2];
-const apiUrl = `${rootUrl}/i18n-json`;
-const appName = process.argv[3];
+const rootUrl = process.argv[2]
+const apiUrl = `${rootUrl}/i18n-json`
+const appName = process.argv[3]
 
 if (!rootUrl || !appName) {
-  throw new Error('rootUrl 和 appName 都必须通过命令行参数传入');
+  throw new Error('rootUrl 和 appName 都必须通过命令行参数传入')
 }
+axios(`${apiUrl}?app_name=${appName}`).then((response) => {
+  const data = response.data
 
-const response = await axios(`${apiUrl}?app_name=${appName}`);
-const data = response.data;
-
-fs.writeFileSync(path.resolve(process.cwd(), 'src/locales/i18n.json'), JSON.stringify(data, null, 2));
+  const filePath = path.resolve(process.cwd(), 'src/locales/i18n.json')
+  const dirPath = path.dirname(filePath)
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true })
+  }
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2))
+})
